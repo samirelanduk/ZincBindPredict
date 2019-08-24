91580927e7c4bd52c3588149080c0065afe4a3bf
Sam Ireland <sam.ireland.uk@gmail.com>
2019-08-24 13:51:27 +0100

    New multi-model build script

---
+++ predict/build.py
@@ -0,0 +1,36 @@
+import os
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+import joblib
+from config import CONFIG
+
+# What families are there?
+csvs = [csv[:-4] for csv in os.listdir("data/csv") if csv.endswith("csv")]
+
+# Go through each family
+for csv in csvs:
+    print(f"Processing {csv}...")
+    # Load the dataset and save it as a numpy array
+    data = pd.read_csv(f"data/csv/{csv}.csv").values
+    np.save(f"predict/npy/{csv}", data)
+
+    # Split into train and test data, and save
+    data_train, data_test = train_test_split(data)
+    data_train, data_test = data_train[:, 1:], data_test[:, 1:]
+    np.save(f"predict/npy/{csv}_train", data_train)
+    np.save(f"predict/npy/{csv}_test", data_test)
+
+    # Get the models to be used
+    for Model, hyperparameters in CONFIG.items():
+        print("   ", Model.__name__)
+
+        # Create the model with hyperparameters
+        model = Model(**hyperparameters)
+
+        # Train model
+        X_train, y_train = data_train[:, :-1], data_train[:, -1].astype("int")
+        model.fit(X_train, y_train)
+
+        # Save model
+        joblib.dump(model, f"predict/models/{csv}-{Model.__name__}.joblib")
---
+++ predict/config.py
@@ -0,0 +1,15 @@
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+
+CONFIG = {
+    KNeighborsClassifier: {
+
+    },
+    RandomForestClassifier: {
+        "n_estimators": 10
+    },
+    SVC: {
+        "gamma": "scale"
+    }
+}
