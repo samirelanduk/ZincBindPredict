a6b109afad74cc1be25af48486caac5a087e0815
Sam Ireland <sam.ireland.uk@gmail.com>
2019-09-01 18:47:42 +0100

    Sequence data notebook generation

--- data/make_notebooks.py
+++ data/make_notebooks.py
@@ -1,16 +1,19 @@
+#! /usr/bin/env python3
+
 import os
 import json
 
-# What families?
-structure_families = [f[:-4] for f in os.listdir("data/csv/structure") if f.endswith("csv")]
+for category in ["structure", "sequence"]:
+    # What families?
+    families = [f[:-4] for f in os.listdir(f"data/csv/{category}") if f.endswith("csv")]
 
-# Get notebook template
-with open("data/notebooks/structure-template.json") as f:
-    template = f.read()
+    # Get notebook template
+    with open(f"data/notebooks/{category}-template.json") as f:
+        template = f.read()
 
-
-for family in structure_families:
-    with open(f"data/notebooks/{family}-structure.ipynb", "w") as f:
-        f.write(template.replace("X1", family))
+    # Make notebooks
+    for family in families:
+        with open(f"data/notebooks/{family}-{category}.ipynb", "w") as f:
+            f.write(template.replace("X1", family))
 
 
