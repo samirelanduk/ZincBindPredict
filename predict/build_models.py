f9b9c781ac870a7491751f05f6551c84517c24df
Sam Ireland <sam.ireland.uk@gmail.com>
2020-01-07 18:53:08 +0000

    Enable loading precalculated hyperparameters

--- predict/build_models.py
+++ predict/build_models.py
@@ -2,6 +2,7 @@
 
 import sys
 import os
+import json
 import pandas as pd
 import numpy as np
 import joblib
@@ -14,6 +15,7 @@ from models import MODELS
 categories = ["structure", "sequence"]
 families = open("data/families.dat").read().splitlines()
 proportion = 1
+loadhp = False
 for arg in sys.argv:
     if arg.startswith("--categories="):
         categories = [c for c in arg[13:].split(",") if c in categories]
@@ -25,8 +27,14 @@ for arg in sys.argv:
         proportion = float(arg[13:])
         assert 0 < proportion <= 1
         continue
+    if arg.startswith("--loadhp"):
+        loadhp = True
+        continue
 
 results = {}
+try:
+    with open("results.json") as f: old_results = json.load(f)
+except: old_results = {}
 
 for category in categories:
     print(f"{category} models")
@@ -60,24 +68,37 @@ for category in categories:
             hp_space = MODELS[Model]["hyperparameters"]
 
             # Which hyperparameters give the best validation F1 score?
-            print("            Searching hyperparameter space...")
-            hp_results = []
-            for combo in product(*hp_space.values()):
-                hyperparameters = dict(zip(hp_space.keys(), combo))
+            hyperparameters = None
+            if loadhp:
+                try:
+                    hyperparameters = old_results[category][family][model_name]["hyperparameters"]
+                except: pass
+            if hyperparameters:
+                print("            Cross-validating with precalculated hyperparameters...")
                 model = Model(**hyperparameters)
                 scores = cross_validate(model, X_train, y_train, cv=5, scoring=["recall", "precision", "f1"])
-                hp_results.append((scores["test_f1"].mean(), hyperparameters))
-            hyperparameters = max(hp_results, key=lambda h: h[0])[1]
-            validation_recall = scores["test_recall"].mean()
-            validation_precision = scores["test_precision"].mean()
+                validation_recall = scores["test_recall"].mean()
+                validation_precision = scores["test_precision"].mean()
+                results[category][family][model_name]["hyperparameters"] = hyperparameters
+            else:
+                print("            Searching hyperparameter space...")
+                hp_results = []
+                for combo in product(*hp_space.values()):
+                    hyperparameters = dict(zip(hp_space.keys(), combo))
+                    model = Model(**hyperparameters)
+                    scores = cross_validate(model, X_train, y_train, cv=5, scoring=["recall", "precision", "f1"])
+                    hp_results.append((scores["test_f1"].mean(), hyperparameters))
+                hyperparameters = max(hp_results, key=lambda h: h[0])[1]
+                results[category][family][model_name]["hyperparameters"] = hyperparameters
+                validation_recall = scores["test_recall"].mean()
+                validation_precision = scores["test_precision"].mean()
 
             # Create learning curve
             print("            Calculating learning curve...")
             model = Model(**hyperparameters)
             train_sizes = [x for x in [
                 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.08, 0.1, 0.2, 0.3, 0.4, 0.6, 0.8, 1
-            ] if 1 / (800 * x) <= proportion]
-            print(train_sizes)
+            ] if 1 / (40 * x) <= proportion]
             scores = learning_curve(model, X_train, y_train, train_sizes=train_sizes, scoring="f1", cv=5, n_jobs=-1)
             plt.plot(scores[0], [v.mean() for v in scores[1]], label="Train F1")
             plt.plot(scores[0], [v.mean() for v in scores[2]], label="Test F1")
@@ -119,6 +140,9 @@ for category in categories:
             model._test_recall = results[category][family][model_name]["recall"] = test_recall
             model._test_precision = results[category][family][model_name]["precision"] = test_precision
             joblib.dump(model, f"predict/models/{category}/{family}-{model_name.replace(' ', '-')}.joblib")
+            if "SVM" in model_name:
+                del results[category][family][model_name]["hyperparameters"]["probability"]
+            with open("results.json", "w") as f: json.dump(results, f, indent=4)
 
 
 print("Final Metrics")
@@ -129,4 +153,5 @@ for category in results:
         for model in results[category][family]:
             print("     ", model)
             for metric in results[category][family][model]:
-                print(f"        {metric}: {round(results[category][family][model][metric], 3)}")
+                if metric != "hyperparameters":
+                    print(f"        {metric}: {round(results[category][family][model][metric], 3)}")
